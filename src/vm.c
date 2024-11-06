604286d9fd09cb22311b0fa3da7b32743ac3f43e
Parth Dundh <dundh.parth@gmail.com>
2024-11-06 19:12:59 +0000

    Implementing stack for virtual machine

--- src/vm.c
+++ src/vm.c
@@ -3,15 +3,29 @@
 #include <stdint.h>
 #include "../include/vm.h"
 #include <stdio.h>
-
+#include "../include/debug.h"
 VM vm;
 
-void initVM(){
+static void resetStack(){
+  vm.stackTop = vm.stack;
+}
 
+void initVM(){
+  resetStack();
 }
 
 void freeVM(){
 
+}
+
+void push(Value value){
+  *vm.stackTop = value;
+  vm.stackTop ++;
+}
+
+Value pop(){
+  vm.stackTop --;
+  return *vm.stackTop;
 }
 
 static InterpretResult run(){
@@ -19,6 +33,19 @@ static InterpretResult run(){
   #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for(;;){
+    #ifdef DEBUG_TRACE_EXECUTION
+      printf("    ");
+
+      for(Value* slot = vm.stack; slot < vm.stackTop; slot++){
+        printf("[");
+        printValue(*slot);
+        printf("]");
+      }
+
+      printf("\n");
+      disassembleInstruction(vm.chunk, (int)( vm.ip - vm.chunk->code ));
+    #endif
+
     uint8_t instruction;
     switch(instruction = READ_BYTE()){
 
@@ -27,8 +54,7 @@ static InterpretResult run(){
 
       case OP_CONSTANT:{
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
     }
